739ea4bf6e2d833168e387488d5a545ed94ba11e
Han <phamhan@uw.edu>
2024-12-02 20:20:55 -0800

    day 2: completed

--- 2024/2/AttemptTwo.swift
+++ 2024/2/AttemptTwo.swift
@@ -5,16 +5,13 @@ import Foundation
 class Solution {
     let input: String
     var unsafePart1: Int
-    var unsafePart2: Int
 
     init(input: String) {
         self.input = input
         unsafePart1 = 0
-        unsafePart2 = 0
     }
 
     // MARK: - Part 1
-
     func solvePart1() -> Int {
         let reports = input.split(separator: "\n").map(String.init)
         let reportsN = reports.count
@@ -60,7 +57,8 @@ class Solution {
 
         for i in 0..<levels.count - 1 {
             let diff = levels[i + 1] - levels[i]
-            if diff < 1 || diff > 3 {return false}
+            let absDiff = abs(levels[i + 1] - levels[i])
+            if absDiff < 1 || absDiff > 3 {return false}
             if diff > 0 { isDecreasing = false }
             if diff < 0 { isIncreasing = false }
         }
@@ -69,67 +67,46 @@ class Solution {
     }
 
     func solvePart2() -> Int {
-        let reports = input.split(separator: "\n").map(String.init)
-        let reportsN = reports.count
+        let reports = input.split(separator: "\n").map{$0.split(separator: " ").compactMap {Int($0)}}
+        // compactMap
+        // * converts each substring to an Int
+        // * numbers that couldn't be converted are omitted
+
         var safeCount = 0
 
         for report in reports {
+            print(report)
+            // check if the report is safe
             if isSafe(report) {
+                print("I'm safe")
                 safeCount += 1
                 continue
             }
-            var tracker: [Bool] = [false, false, false]
-            var problemDampenerUsed = false
-            let levels: [Substring] = report.split(separator: " ")
-            for i in 0 ..< levels.count - 1 {
-                if let firstNum = Int(levels[i]), let secondNum = Int(levels[i + 1]) {
-                    // check if increasing or decreasing
-                    let levelDifference = firstNum - secondNum
-                    if levelDifference > 0 {
-                        tracker[0] = true
-                    } else if levelDifference < 0 {
-                        tracker[1] = true
-                    } else {
-                        if problemDampenerUsed {
-                            unsafePart2 += 1
-                            break
-                        }
-                        problemDampenerUsed = true
-                        continue
-                    }
-                    // if the level is both increasing and decreasing
-                    // then it is not safe => break
-                    if tracker[0] == true && tracker[1] == true {
-                        if problemDampenerUsed {
-                            unsafePart2 += 1
-                            break
-                        }
-                        problemDampenerUsed = true
-                        continue
-                    } else {
-                        // check if the abs difference is more than 3
-                        let levelAbsDifference = abs(firstNum - secondNum)
-                        if levelAbsDifference > 3 && problemDampenerUsed {
-                            unsafePart2 += 1
-                            break
-                        } else if levelAbsDifference > 3 && !problemDampenerUsed {
-                            problemDampenerUsed = true
-                            continue
-                        }
-                    }
+
+            var isSafeWithDampener = false
+            for i in 0 ..< report.count {
+                var modifiedReport = report
+                modifiedReport.remove(at: i)
+                print("Removed \(i)")
+                if isSafe(modifiedReport) {
+                    isSafeWithDampener = true
+                    break
                 }
             }
+
+            if isSafeWithDampener {
+                print("I'm safe. I'm the special level")
+                safeCount += 1
+            }
         }
-        print("Safe: \(reportsN - unsafePart2)")
-        return reportsN - unsafePart2
-    }
+        print("Safe: \(safeCount)")
+        return  safeCount
+        }
 }
 
 // MARK: - File Reading
-
-func readInput(isTest: Bool = false) -> String {
-    let filename = isTest ? "test" : "input"
-    let fileURL = URL(fileURLWithPath: filename + ".txt")
+func readInput(from fileName: String) -> String {
+    let fileURL = URL(fileURLWithPath: fileName)
 
     do {
         return try String(contentsOf: fileURL, encoding: .utf8)
@@ -139,37 +116,24 @@ func readInput(isTest: Bool = false) -> String {
     }
 }
 
-// MARK: - Testing
-
-func runTests() {
-    let testInput = readInput(isTest: true)
-    let solution = Solution(input: testInput)
-
-    // Add your test assertions here
-    let part1Result = solution.solvePart1()
-    print("Part 1 Test Result: \(part1Result)")
-    // assert(part1Result == expectedPart1Result, "Part 1 test failed")
-
-    let part2Result = solution.solvePart2()
-    print("Part 2 Test Result: \(part2Result)")
-    // assert(part2Result == expectedPart2Result, "Part 2 test failed")
-}
-
-// MARK: - Main
 
 func main() {
-    // First run tests
-    print("Running tests...")
-    runTests()
-    print("Tests completed!")
+    // get command line args
+    let args = CommandLine.arguments
+    guard args.count > 1 else {
+        print("Usage: swift solution.swift <input_file>")
+        return
+    }
 
-    // Then solve actual puzzle
-    print("\nSolving puzzle...")
-    let input = readInput()
+    let inputFile = args[1]
+    print("Using input file: \(inputFile)")
+
+    // run solutions
+    let input = readInput(from: inputFile)
     let solution = Solution(input: input)
 
-    let part1 = solution.solvePart1()
-    print("Part 1 Solution: \(part1)")
+    // let part1 = solution.solvePart1()
+    // print("Part 1 Solution: \(part1)")
 
     let part2 = solution.solvePart2()
     print("Part 2 Solution: \(part2)")
